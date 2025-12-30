b0f8f7004c934a20fc9c02853bb433fccabe69f7
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-12-30 14:27:11 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from "react"; 
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects";
-import "./index.css";
+import { Projects } from "./components/sections/Projects"; 
 import { Contact } from "./components/sections/Contact";
 import { Achievement } from "./components/sections/Acheivement";
 
