2ffa9744312e30016d992217b09624b0849d71a0
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-12-31 12:57:12 +0530

    commit new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"; 
+import { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects"; 
-import { Contact } from "./components/sections/Contact"; 
+import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 import { Achievement } from "./components/sections/Acheivement";
 
 function App() {
@@ -16,15 +16,14 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
+        className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
+          } bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
-        <Achievement /> 
+        <Achievement />
         <Projects />
         <Contact />
       </div>
